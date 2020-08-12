ae0fa32f44a038879191ed5c8e5e806f0ece6c92
Randall Hall <randallhall@icloud.com>
2020-08-11 23:51:19 -0500

    Update covid_table_schema.sql

--- dallas_county_covid_19/covid_table_schema.sql
+++ dallas_county_covid_19/covid_table_schema.sql
@@ -2,6 +2,7 @@ CREATE TABLE dallas_covid (
 id SERIAL PRIMARY KEY,
 new_cases integer NOT NULL,
 total_cases integer NOT NULL,
+new_deaths integer NOT NULL,
 total_deaths integer NOT NULL,
 risk_level varchar(25) NOT NULL,
 county varchar(255) NOT NULL,
